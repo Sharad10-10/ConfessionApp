2f2e44869c6ccea05a546cd805ca910bd076929f
Sharad Ghimire <sharadghimire@Sharads-MacBook-Air.local>
2025-04-16 18:50:10 -0600

    changes made in API Key

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD9AG5hgE0NFaklstSJULgctk05pp5O5Po",
+  apiKey: "************************",
   authDomain: "confessionapp-586f0.firebaseapp.com",
   projectId: "confessionapp-586f0",
   storageBucket: "confessionapp-586f0.firebasestorage.app",
